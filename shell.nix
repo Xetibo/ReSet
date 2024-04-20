39c338246fb545e048728659ba068514b332720d
DashieTM <fabio.lenherr@gmail.com>
2024-04-20 10:14:14 +0200

    chore: Change nix depencendies

--- shell.nix
+++ shell.nix
@@ -1,16 +1,19 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 with pkgs;
 mkShell {
   nativeBuildInputs = [
     pkg-config
+    wrapGAppsHook4
   ];
 
   buildInputs = [
     dbus
+    pulseaudio
+    gnome.adwaita-icon-theme
     gtk4
     libadwaita
-    pulseaudio
+    gdk-pixbuf
   ];
 
 }
